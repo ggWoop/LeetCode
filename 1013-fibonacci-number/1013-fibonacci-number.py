7504b8acec354422118cdaba9c15fa186be68332
ggWoop <131653498+ggWoop@users.noreply.github.com>
2023-08-16 09:11:03 +0900

    Time: 36 ms (91.17%), Space: 16.3 MB (25.73%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        dp = [0,1]
+
+        if n<= 1 :
+            return dp[n]
+
+        for i in range(n-1):
+            dp.append(dp[i] + dp[i+1])
+        return dp[-1]
